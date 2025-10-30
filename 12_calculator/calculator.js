2917c2de70bf8e5baf090b8c82e6496eb8ed4fda
Leng Keong <kenshih_keong@hotmail.com>
2025-10-30 14:42:18 +0800

    Complete all exercises

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return Number(array.reduce((accumulator,currentValue)=>accumulator+currentValue,0));
+  // return array.reduce((total, current) => total + current, 0);
+  // return args.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((a,b)=> a*b);
 };
 
-const power = function() {
-	
+const power = function(base,exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let result = 1;
+  for (let index = number; number != 0; number--) {
+    result*=number;
+  }
+  return result;
 };
 
 // Do not edit below this line
